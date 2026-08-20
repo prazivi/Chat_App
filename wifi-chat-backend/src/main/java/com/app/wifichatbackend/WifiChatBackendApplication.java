d2d758ca3a0f8beffb6902f9207e9b734e9c099c
Pranav Gupta <kpranav.in@gmail.com>
2026-08-21 00:12:06 +0530

    first commit

---
+++ wifi-chat-backend/src/main/java/com/app/wifichatbackend/WifiChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.wifichatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WifiChatBackendApplication.class, args);
+    }
+
+}
---
+++ wifi-chat-backend/src/test/java/com/app/wifichatbackend/WifiChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wifichatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
